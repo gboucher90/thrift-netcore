54c830bced86b7f7532c2347f71d76faa8ed0cdb
Guillaume <guillaume1998@free.fr>
2017-07-09 18:23:16 +0200

    Use csproj instead of project.json

--- build.sh
+++ build.sh
@@ -9,8 +9,9 @@ if [ -d $artifactsFolder ]; then
   rm -R $artifactsFolder
 fi
 
-dotnet restore
+dotnet restore thrift.sln
+dotnet build  -c Release thrift.sln
 cd ./test/JSON/
-dotnet test -c Release -f netcoreapp1.0
+dotnet test -c Release JSON.csproj
 cd ../..
-dotnet pack ./src/thrift-netcore -c Release -o ./artifacts
+dotnet pack ./src/thrift-netcore/thrift-netcore.csproj -c Release -o ./artifacts
