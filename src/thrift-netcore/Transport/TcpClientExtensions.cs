6662724f2b8e37518b76b4616d39b107da88053a
anthonywanted <anthonywanted@163.com>
2017-10-30 12:26:09 +0800

    check if Dns.GetHostAddressesAsync got a ip "::1" then convert to 127.0.0.1

--- src/thrift-netcore/Transport/TcpClientExtensions.cs
+++ src/thrift-netcore/Transport/TcpClientExtensions.cs
@@ -19,11 +19,15 @@ namespace Thrift.Transport
             }
             else
             {
-                IPAddress[] address = await Dns.GetHostAddressesAsync(host);// not ip, resolve it and get IP(s)
-                if (address.Length > 0)
+                IPAddress[] addresses = await Dns.GetHostAddressesAsync(host);// not ip, resolve it and get IP(s)
+                
+                if (addresses.Length > 0)
                 {
-                    int rand = (int)(DateTime.Now.Ticks % address.Length); // if dns port to multiple IPs, random an ip to connect
-                    await tcpClient.ConnectAsync(address[rand], port);
+                    int rand = (int)(DateTime.Now.Ticks % addresses.Length); // if dns port to multiple IPs, random an ip to connect
+                    IPAddress address = addresses[rand];
+                    if (address.ToString() == "::1")
+                        address = IPAddress.Parse("127.0.0.1");
+                    await tcpClient.ConnectAsync(address, port);
                 }
                 else
                     throw new TTransportException(TTransportException.ExceptionType.NotOpen, $"Can't resolve host({host})");
