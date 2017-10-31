aa92030e86b4f57116bca63cb9cb231e51e01106
anthonywanted <anthonywanted@163.com>
2017-10-31 12:53:23 +0800

    1. Dns.GetHostAddressesAsync not support ipv6, so filter it 2. change Date.Now to DateTime.UtcNow 3. move     <PackageReference Include="System.Net.NameResolution" Version="4.3.0" />    to PackageReference ItemGroup

--- src/thrift-netcore/Transport/TcpClientExtensions.cs
+++ src/thrift-netcore/Transport/TcpClientExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -13,24 +14,32 @@ namespace Thrift.Transport
         {
 #if NETSTANDARD1_4 || NETSTANDARD1_5
             IPAddress ip;
-            if (IPAddress.TryParse(host, out ip)) // if host is ip, parse it and connect
+            // if host is ip, parse it and connect
+            if (IPAddress.TryParse(host, out ip)) 
             {
                 await tcpClient.ConnectAsync(ip, port);
             }
             else
             {
-                IPAddress[] addresses = await Dns.GetHostAddressesAsync(host);// not ip, resolve it and get IP(s)
-                
+                // not ip, resolve it and get IP(s)
+                IPAddress[] addresses = await Dns.GetHostAddressesAsync(host);
+                // tcpClient is not compatible for ipv6(in InitSocket()), so filter it
+                // or consider changing InitSocket() to:
+                // client = new TcpClient(AddressFamily.InterNetworkV6);
+                // client.Client.DualMode = true;
+                // it can support both ipv4 and ipv6, but it will cause delaying the time to establish the connection if the host is not listening on the IPv6 address
+                // https://technet.microsoft.com/en/library/hh138320(v=vs.110)
+                addresses = addresses.Where(p => p.AddressFamily == AddressFamily.InterNetwork).ToArray();
+
                 if (addresses.Length > 0)
                 {
-                    int rand = (int)(DateTime.Now.Ticks % addresses.Length); // if dns port to multiple IPs, random an ip to connect
+                    // if dns port to multiple IPs, random an ip to connect
+                    int rand = (int)(DateTime.UtcNow.Ticks % addresses.Length); 
                     IPAddress address = addresses[rand];
-                    if (address.ToString() == "::1")
-                        address = IPAddress.Parse("127.0.0.1");
                     await tcpClient.ConnectAsync(address, port);
                 }
                 else
-                    throw new TTransportException(TTransportException.ExceptionType.NotOpen, $"Can't resolve host({host})");
+                    throw new TTransportException(TTransportException.ExceptionType.NotOpen, $"Can't resolve host({host}) to a ipv4's ip");
             }
 #else
             await client.ConnectAsync(host, port);
